548f9223a30be303b6335ec99d9ea53ccc84da95
npv34 <phanvanluan611996@gmail.com>
2021-10-18 15:30:17 +0700

    demo library front-end

---
+++ src/app/admin/users/user-list/user-list.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit} from '@angular/core';
+import {IUser} from "../IUser";
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+  users: IUser[] = [
+    {
+      username: "admin",
+      email: "admin@gmail.com",
+      phone: "0909989898",
+      avatar: "https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png"
+    },
+    {
+      username: "cuong",
+      email: "cuong@gmail.com",
+      phone: "0909989898",
+      avatar: "https://www.w3schools.com/howto/img_avatar2.png"
+    },
+    {
+      username: "hung",
+      email: "admin@gmail.com",
+      phone: "0909989898",
+      avatar: "https://teachingandlearning.schulich.yorku.ca/wp-content/uploads/2019/10/avatar6.png"
+    }
+  ]
+  pageTitle = "Danh sách người dùng";
+  btnClass = "btn btn-danger";
+  imageSize = "150"
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  delete(id: any) {
+    if (confirm('Are you sure?')) {
+      this.users.splice(id, 1);
+    }
+  }
+
+  resizeImage(event: any) {
+    let value = event.target.value;
+    this.imageSize = value;
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layouts/core/navbar/navbar.component';
+import { FooterComponent } from './layouts/core/footer/footer.component';
+import { UserListComponent } from './admin/users/user-list/user-list.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
